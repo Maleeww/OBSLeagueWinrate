2b2405e7e3848acedb7d2f2d070bb6119c47ec56
Maleeww <dvdalco2@gmail.com>
2022-09-15 01:40:59 +0200

    Change folder structure express

--- public/javascripts/server.js
+++ public/javascripts/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'));
-});
+
+app.use(express.static('public'));
+/* app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../index.html'));
+}); */
 
 const port = 25564;
 const server = app.listen(port, () => {
